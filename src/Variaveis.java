85553cdba4b9122b5652120173e576430730fbf5
lucascorrea1 <rs.lucascorrea@gmail.com>
2022-03-15 21:24:18 -0300

    Traduzindo os comentários para PT-BR e com as minhas palavras, para assim, fixar melhor o conteúdo.

--- src/Variaveis.java
+++ src/Variaveis.java
@@ -6,7 +6,7 @@ public class Variaveis {
         boolean myBool = true;
         String myText = "Hello";
 
-        //  Display Variables
+        //  Imprime as variáveis
         System.out.println("My Num: "+myNum);
         System.out.println("My Float Num: "+myFloatNum);
         System.out.println("My Char: "+myLetter);
@@ -18,25 +18,15 @@ public class Variaveis {
         String fullName = firstName + lastName;
         System.out.println("My Full Name: "+fullName);
 
-        //  To declare more than one variable of the same type, use a comma-separated list:
+        //  Para declarar mais de uma variável do mesmo tipo, usamos a vírgula.
         int x = 5, y = 6, z = 50;
         System.out.println(x + y + z);
 
-        //  All Java variables must be identified with unique names.
-        // Good
+        //  Todas as variáveis no Java devem ter um nome único.
+        //  Bom
         int minutesPerHour = 60;
 
-        // OK, but not so easy to understand what m actually is
+        //  Funciona, mas não é fácil de entender o que 'm' significa.
         int m = 60;
-
-        /*
-        * The general rules for naming variables are:
-            Names can contain letters, digits, underscores, and dollar signs
-            Names must begin with a letter
-            Names should start with a lowercase letter and it cannot contain whitespace
-            Names can also begin with $ and _ (but we will not use it in this tutorial)
-            Names are case sensitive ("myVar" and "myvar" are different variables)
-            Reserved words (like Java keywords, such as int or boolean) cannot be used as names
-        * */
     }
 }
