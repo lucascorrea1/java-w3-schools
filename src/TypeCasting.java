af584373b0a4ca976284ae52d4445bdeac4602be
lucascorrea1 <rs.lucascorrea@gmail.com>
2022-03-15 21:35:27 -0300

    Resumindo e traduzindo os comentários para PT-BR, para assim, fixar melhor o conteúdo.

--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -1,22 +1,20 @@
 public class TypeCasting {
     public static void main(String[] args) {
-
-        //Widening Casting
-        //is done automatically when passing a smaller size type to a larger size type:
+        //  Widening Casting
+        //  Ocorre quando passamos um tipo de tamanho menor para um tipo de tamanho maior
         int myInt = 9;
-        double myDouble = myInt; // Automatic casting: int to double
+        double myDouble = myInt; //  transforma int em double
 
-        System.out.println(myInt);      // Outputs 9
-        System.out.println(myDouble);   // Outputs 9.0
+        System.out.println(myInt);      //  Saída: 9
+        System.out.println(myDouble);   //  Saída: 9.0
 
-
-        //Narrowing Casting
-        //must be done manually by placing the type in parentheses in front of the value:
+        //  Narrowing Casting
+        //  Feito colocando o tipo entre parênteses na frente do valor
         double myDouble2 = 9.78d;
-        int myInt2 = (int) myDouble; // Manual casting: double to int
+        int myInt2 = (int) myDouble2; //  Transforma double em int
 
-        System.out.println(myDouble2);   // Outputs 9.78
-        System.out.println(myInt2);      // Outputs 9
+        System.out.println(myDouble2);   //  Saída: 9.78
+        System.out.println(myInt2);      //  Saída: 9
 
     }
 }
