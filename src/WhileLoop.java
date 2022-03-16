dbf296ef708e19357643b32bd1d30a6837dc86dd
lucascorrea1 <rs.lucascorrea@gmail.com>
2022-03-15 21:29:54 -0300

    Resumindo e traduzindo os comentários para PT-BR, para assim, fixar melhor o conteúdo.

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,26 +1,22 @@
 public class WhileLoop {
 
-    // Loops can execute a block of code as long as a specified condition is reached.
-    // Loops are handy because they save time, reduce errors, and they make code more readable.
-
     public static void main(String[] args) {
-        //  While Loop
-        // The while loop loops through a block of code as long as a specified condition is true:
+
+        // Se uma condição especificada for alcançada o loop pode executar um bloco de código.
         int i = 0;
         while (i < 5) {
             System.out.println(i);
             i++;
         }
 
-        //  Do/While Loop
-        // This loop will execute the code block once, before checking if the condition is true,
-        // then it will repeat the loop as long as the condition is true
+        //  Executará o bloco de código uma vez, antes mesmo de verificar se a condição é verdadeira,
+        //  Então ele irá continuar o loop enquanto a condição for verdadeira
         int x = 0;
         do {
             System.out.println(i);
             x++;
         }
         while (x < 5);
-
     }
+
 }
